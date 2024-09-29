40fe0ee19f703b66fd45d11d59f704557e5e6d06
Isaac1676 <ezechielncho225@gmail.com>
2024-09-29 22:40:29 +0000

    fix images

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import Logo from '../../assets/exchange.svg';
+import Logo from '../../assets/colorkit.svg';
 
 const NavBar = () => {
     return (
         <nav className='flex justify-between px-12 py-7 items-center border-solid sticky top-0 z-50'>
             <div>
                 <NavLink to='/' className='flex flex-row justify-between items-center gap-4'>
-                    <div className='bg-[#0E3969] rounded-2xl w-12 h-12 flex justify-center items-center'>
-                        <img src={Logo} alt="" height={35} width={35} />
-                    </div>
+                    <img src={Logo} alt="" height={35} width={35} />
                     <span className='text-[#0E3969] font-medium text-2xl'>FileSA</span>
                 </NavLink>
             </div>
